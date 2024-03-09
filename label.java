ef8c200bb11f65291ac2e1d302f4b5d5ebb39075
riti2601 <ritishapande@gmail.com>
2024-03-09 15:47:11 +0530

    label done

--- label.java
+++ label.java
@@ -22,12 +22,14 @@ public class label {
         label.setVerticalTextPosition(JLabel.TOP);// top, center, bottom of imageicon
 
         label.setForeground(new Color(100,10,0)); // change color of the text ie font color 
-        label.setFont(new Font("MV Boli", Font.PLAIN,20));//set font of the text, its size and italic thingie
+        label.setFont(new Font("MV Boli", Font.PLAIN,50));//set font of the text, its size and italic thingie
         label.setIconTextGap(10); // set gap of text to image
         label.setBackground(Color.BLACK); 
         label.setOpaque(true);
         label.setVerticalAlignment(JLabel.CENTER); // sets vertical position of icon and text within label
-        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER); // sets horizontal position of icon and text within label
+        //label.setBounds(100,100, 250, 250); //this sets x and y position within frame as well as dimensions
+
 
         label.setBorder(border);// added border to the label
 
@@ -35,9 +37,11 @@ public class label {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
         frame.setVisible(true);
+       // frame.setLayout(null); // to fx the lable at a particular position
         frame.setTitle("lets code");
         frame.add(label);
         
+        frame.pack(); // this is to be used after we have all the components in the frame otherwise it will crete problems , it is used so that whatever is in the frame will become the size of the frame it will fit it to the size
 
         
 
