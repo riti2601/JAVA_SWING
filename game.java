15a5573405cda45ab9a3db81ef3eb067c669010f
riti2601 <ritishapande@gmail.com>
2024-04-22 22:33:34 +0530

    game for patterns

---
+++ game.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class game{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        frame.setLayout(null);
+    }
+}
